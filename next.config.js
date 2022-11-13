42ae0e3e33b1041a5a8d3e8642b9ba76674ac06d
Caroline A Bandeira <carol_bandeira7@hotmail.com>
2022-11-13 18:08:59 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/* const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["cdn.traction.one"],
+  },
+};
+/* module.exports = nextConfig */
